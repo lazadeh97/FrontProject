84f6f80a94f63912223b0dfb1caa24d84a8501b9
lazadeh97 <126696851+lazadeh97@users.noreply.github.com>
2023-09-11 17:27:20 +0400

    all changes.

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// function changeLightofBody() {
+//   document.body.style.backgroundColor = "black";
+// }
+
+const button = document.getElementById("btnLight");
+
+button.addEventListener("click", (event) => {
+  document.body.style.backgroundColor = "black";
+});
